1052309a71354572fbebe5fe57efbe92a0cb5dce
snymax <snymax@gmail.com>
2020-09-26 16:31:24 -0400

    Why wont it build

---
+++ dist/precache-manifest.9b0db04e14bf7c41e41094b4eed10dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee4a0414d6272b0d5a1bd6ae957bcc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a704878d60e4012cb3f",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "71a163049f703c25e6ec",
+    "url": "/static/js/2.c24323f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24323f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a704878d60e4012cb3f",
+    "url": "/static/js/main.80189a6b.chunk.js"
+  },
+  {
+    "revision": "991106e2a668dd08aca3",
+    "url": "/static/js/runtime-main.434d0006.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
